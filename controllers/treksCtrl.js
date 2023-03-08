a1c32bc61cb8a93f286f7d5aa0f51b23dec35e9a
Maxence Ver Poorten <maxence_vp@maxence-vp-1.home>
2023-03-08 17:29:45 +0100

    create treks & create step

--- controllers/treksCtrl.js
+++ controllers/treksCtrl.js
@@ -1,10 +1,54 @@
-const treksModel = require ("../models/treksModel");
+const treksModel = require("../models/treksModel");
+const parcoursModel = require ("../models/parcoursModel");
 
-const treksCtrl = 
-{
-    getTreksList (req, res){},
-    createTrek (req, res){},
-    updateTrek (req, res){},
-    deleteTrek (req, res){}
-}
+const treksCtrl = {
+  async getTreksList(req, res) {
+    const list = await treksModel.find({});
+    if (!list) {
+      return res
+        .status(500)
+        .json({ message: "Une erreur inattendue s'est produite" });
+    }
+    console.log(list);
+    return res.json(list);
+  },
+  createTrek(req, res) {
+    const {
+      beginDate,
+      endDate,
+      /*parcoursID,
+       *guideID,*/
+      minPlaces,
+      maxPlaces,
+    } = req.body;
+
+    // recuperer le nom du parcours ?
+    //let treksSlug = ?.toLowerCase();
+    //treksSlug = treksSLug.replace(" ", "-");
+
+    let newTreks = new treksModel({
+      beginDate: beginDate,
+      endDate: endDate,
+      minPlaces: minPlaces,
+      maxPlaces: maxPlaces,
+      trekID: "TEST01",
+      guideID: "GUIDE01",
+    });
+
+    newTreks.save().then(()=>
+    {
+        return res
+        .status(201)
+        .json({message: "Trek crée"});
+    })
+    .catch((err)=>
+    {
+        console.log(err); 
+        return res
+        .status(422)
+        .json({message: "Une erreur inattendue est survenue"}); 
+    })
+  },
+  updateTrek(req, res) {},
+};
 module.exports = treksCtrl;
